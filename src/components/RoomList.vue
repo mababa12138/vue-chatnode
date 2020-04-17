3967f01868156599f138bcb5fad4baecf1c10627
msy <982232938@qq.com>
2020-04-17 15:30:43 +0800

    配置后台和webpack

--- src/components/RoomList.vue
+++ src/components/RoomList.vue
@@ -4,7 +4,21 @@
     <h4>Channels</h4>
     <hr>
     <el-tabs :tab-position="tabPosition" style="height: 100%">
-      <el-tab-pane> <div slot="label"><i class="el-icon-chat-round"></i></div>房间列表</el-tab-pane>
+      <el-tab-pane> <div slot="label"><i class="el-icon-chat-round"></i></div>
+        <el-table v-if="activeRoom" 
+          ref="multipleTable"
+          :data="this.rooms"
+          :show-header="false"
+          highlight-current-row
+          style="width: 100%">
+          <el-table-column
+              property="name"
+              href="#"
+              @click="onChange(rooms)">
+          </el-table-column>
+        </el-table>
+      </el-tab-pane>
+
       <el-tab-pane> <div slot="label"><i class="el-icon-search"></i></div>查找</el-tab-pane>
       <el-tab-pane> <div slot="label"><i class="el-icon-user"></i></div>用户</el-tab-pane>
     </el-tabs>
@@ -18,7 +32,8 @@ export default {
   name: 'RoomList',
   data(){
     return {
-      tabPosition: 'left'
+      tabPosition: 'left',
+      currentRow: null
     }
   },
   computed: {
@@ -26,7 +41,18 @@ export default {
       'rooms',
       'activeRoom'
     ]),
-  }
+  },
+  mounted:function(){
+    console.log(this.$refs)
+    let activeRow = null;
+    let tableData = this.$refs.multipleTable.data
+    for(let i = 0;i < tableData.length;i++){
+      if(tableData[i].id === this.activeRoom.id){
+        activeRow = i;
+      }
+    }
+    this.$refs.multipleTable.setCurrentRow(this.$refs.multipleTable.data[activeRow]);
+  },
 }
 </script>
 
@@ -36,5 +62,8 @@ i {
 }
 .room-list{
   height: 100%;
+}
+.el-table .choosed-row {
+  background: rgb(64, 158, 255);
 }
 </style>
